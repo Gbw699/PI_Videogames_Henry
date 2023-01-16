20863c14ca4f35154545b4c73d70354243d85632
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-01-16 11:43:32 -0300

    moduls sequelize ready

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,10 +1,29 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.DATE,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
