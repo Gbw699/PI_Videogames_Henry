5f35ef94519a6308c7bd2790151ee27b7fa463ae
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-01-26 17:47:57 -0300

    validation ready

--- client/src/Components/Form/validation.js
+++ client/src/Components/Form/validation.js
@@ -1,8 +1,7 @@
 const regexName = /^[A-Z]+[a-z0-9\s]{3,25}$/;
-//agragar limite de caracteres a la descripción
-const regexDescription = /^[^$%&|<>#]*$/;
+const regexDescription = /^[^$%&|<>#]{15,255}$/;
 const regexReleased = /^\d{4}([-])(0?[1-9]|1[1-2])\1(3[01]|[12][0-9]|0?[1-9])$/;
-const regexRating = /^[0-4]\.[0-9]{0,2}$/;
+const regexRating = /^[0-4](\.+[0-9]{0,2})?$/;
 
 export const validation = (inputs) => {
   const errors = {};
@@ -12,14 +11,14 @@ export const validation = (inputs) => {
   }
   if (!regexDescription.test(inputs.description) && inputs.description) {
     errors.description =
-      "La descripción no debe contener caracteres especiales";
+      "La descripción no debe contener caracteres especiales y debe tener entre 15 y 255 caracteres";
   }
   if (!regexReleased.test(inputs.released) && inputs.released) {
     errors.released = 'El formato de la fecha debe ser: "yyyy-mm-dd"';
   }
   if (!regexRating.test(inputs.rating) && inputs.rating) {
     errors.rating =
-      "El número de rating puede contener hasta 2 decimales, no puede ser negativo o mayor a 5 y debe contener un punto después del primer dígito";
+      "El número de rating mayor a 5 o negativo. Puede contener hasta 2 decimales";
   }
   return errors;
 };
