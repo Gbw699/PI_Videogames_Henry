a0a47d005f2d78d0055fde265e8bf3450df91729
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-01-29 22:35:44 -0300

    aux

--- client/src/Components/Detail/Detail.jsx
+++ client/src/Components/Detail/Detail.jsx
@@ -1,3 +1,5 @@
+import style from "./Detail.module.css";
+import img from "../../Img/lorenzo-herrera-p0j-mE6mGo4-unsplash.jpg";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
@@ -20,32 +22,35 @@ export default function Detail() {
   }, []);
 
   return (
-    <div>
+    <div className={style.imgContainer}>
+      <img className={style.img} src={img} alt="img" />
       <div>
         <h1>{videogame.name}</h1>
         <NavLink to={"/home"}>
           <button>Return to home</button>
         </NavLink>
         <div>
-          <img src={videogame.background_image} alt="img" />
+          <div>
+            <img src={videogame.background_image} alt="img" />
+          </div>
+          <p value="description">{videogame.description_raw}</p>
+          <ul>
+            {videogame.genres?.map((obj) => {
+              return <li key={obj.name}>{obj.name}</li>;
+            })}
+          </ul>
+          <p value="released">{videogame.released}</p>
+          <p value="rating">{videogame.rating}</p>
+          <ul>
+            {videogame.platforms?.map((obj) => {
+              if (obj.platform) {
+                return <li key={obj.platform.name}>{obj.platform.name}</li>;
+              } else {
+                return <li key={obj}>{obj}</li>;
+              }
+            })}
+          </ul>
         </div>
-        <p value="description">{videogame.description_raw}</p>
-        <ul>
-          {videogame.genres?.map((obj) => {
-            return <li key={obj.name}>{obj.name}</li>;
-          })}
-        </ul>
-        <p value="released">{videogame.released}</p>
-        <p value="rating">{videogame.rating}</p>
-        <ul>
-          {videogame.platforms?.map((obj) => {
-            if (obj.platform) {
-              return <li key={obj.platform.name}>{obj.platform.name}</li>;
-            } else {
-              return <li key={obj}>{obj}</li>;
-            }
-          })}
-        </ul>
       </div>
     </div>
   );
