c05fbd931a2a11492ef002e6f6dfe670710f7eae
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-02-03 02:35:25 -0300

    aux

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "pi-videogames-henry-dk66im5yv-gbw699.vercel.app"
+    "https://pi-videogames-henry-gbw699.vercel.app/"
   ); // update to match the domain you will make the request from
   //res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
